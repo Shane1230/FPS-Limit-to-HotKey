c3c91c0b29cd8b63ac823c9b351096e602f041dd
Shane <dddiibbb@naver.com>
2025-08-14 05:55:25 +0900

    1.1

--- keybinds/45.lua
+++ keybinds/45.lua
@@ -1,10 +1,5 @@
-setup:set_fps_cap(45) -- set frame rate
-managers.user:set_setting("fps_cap", 45) -- sync to video setting
+ setup:set_fps_cap(45) -- set frame rate
+--managers.user:set_setting("fps_cap", 45) -- sync to video setting
 
 local fps_limit = managers.localization:text("menu_fps_limit")
-if FPSHK.Options.show_on_hud == true then
-	managers.hud:show_hint({text = fps_limit .. ": 45"})
-end
-if FPSHK.Options.show_in_chat == true then
-	managers.chat:feed_system_message(ChatManager.GAME, fps_limit .. ": 45")
-end
+managers.hud:show_hint({text = fps_limit .. ": 45"})
--- keybinds/unlimited.lua
+++ keybinds/unlimited.lua
@@ -1,11 +1,6 @@
 setup:set_fps_cap(unlimited) -- set frame rate
-managers.user:set_setting("fps_cap", unlimited) -- sync to video setting
+--managers.user:set_setting("fps_cap", unlimited) -- sync to video setting
 
 local fps_limit = managers.localization:text("menu_fps_limit")
 local fps_unlimited = managers.localization:text("menu_limit_unlimited")
-if FPSHK.Options.show_on_hud == true then
-	managers.hud:show_hint({text = fps_limit .. ": " .. fps_unlimited})
-end
-if FPSHK.Options.show_in_chat == true then
-	managers.chat:feed_system_message(ChatManager.GAME, fps_limit .. ": " .. fps_unlimited)
-end
+managers.hud:show_hint({text = fps_limit .. ": " .. fps_unlimited})
