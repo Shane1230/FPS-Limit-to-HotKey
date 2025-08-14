080ef688b002cb4ad0f53d63788dca6219947b20
Shane <dddiibbb@naver.com>
2025-08-14 23:46:55 +0900

    1.1

--- keybinds/120.lua
+++ keybinds/120.lua
@@ -1,10 +1,5 @@
 setup:set_fps_cap(120) -- set frame rate
-managers.user:set_setting("fps_cap", 120) -- sync to video setting
+--managers.user:set_setting("fps_cap", 120) -- sync to video setting
 
 local fps_limit = managers.localization:text("menu_fps_limit")
-if FPSHK.Options.show_on_hud == true then
-	managers.hud:show_hint({text = fps_limit .. ": 120"})
-end
-if FPSHK.Options.show_in_chat == true then
-	managers.chat:feed_system_message(ChatManager.GAME, fps_limit .. ": 120")
-end
+managers.hud:show_hint({text = fps_limit .. ": 120"})
